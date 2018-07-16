bcfa57c9fc8065629b510f63016bcb8ca00f37e7
SowleMan <tsowle1423@gmail.com>
2018-07-15 21:38:04 -0500

    Add: CatHealth and PlayerAttack

---
+++ Game Jam/Assets/CatHealth.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatHealth : MonoBehaviour {
+
+	public int startingHealth = 100;
+	public int currentHealth;
+	public float sinkSpeed;
+
+	CapsuleCollider capsuleCollider;
+	bool isDead;
+	bool isSinking;
+
+	// Use this for initialization
+	void Start () {
+		capsuleCollider = GetComponent<CapsuleCollider> ();
+
+		currentHealth = startingHealth;
+		Debug.Log("Cat health started");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isSinking) {
+			transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);
+		}
+	}
+
+	public void TakeDamage(int amount) {
+		Debug.Log("**Take Damage Called**");
+
+		if (isDead) {
+			return;
+		}
+
+		currentHealth -= amount;
+
+		if (currentHealth <= 0) {
+			Death ();
+		}
+
+	}
+
+	void Death() {
+		Debug.Log("**Death Called**");
+		isDead = true;
+
+		capsuleCollider.isTrigger = true;
+	}
+
+	public void startSinking() {
+		//Get and disable the Nav Mesh Agent
+		// GetComponent ().enabled = false;
+
+		//Find the rigidBody component and make it kinetic
+		//GetComponent.isKinematic = true;
+
+		isSinking = true;
+		Debug.Log("Start Sinking");
+
+		Destroy (gameObject, 2f);
+	}
+}
---
+++ Game Jam/Assets/PlayerAttack.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+	public float timeBetweenAttacks = 1f;
+	public int attackDamage = 10;
+
+	GameObject cat;
+	CatHealth catHealth;
+	bool catInRange;
+	float timer;
+
+	// Use this for initialization
+	void Start () {
+		cat = GameObject.FindGameObjectWithTag ("Cat");
+		catHealth = cat.GetComponent<CatHealth> ();
+		Debug.Log("Player Attack Started");
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log("Cat in range check", other.gameObject);
+		if (other.gameObject == cat) {
+			Debug.Log("**Cat is in range**");
+			catInRange = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		Debug.Log("Cat out of range check", other.gameObject);
+		if (other.gameObject == cat) {
+			catInRange = false;
+		}
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+
+		if (timer >= timeBetweenAttacks && catInRange && catHealth.currentHealth > 0) {
+			Debug.Log("**Atack**");
+			Attack ();
+		}
+
+		if (catHealth.currentHealth <= 0) {
+			//cat is dead, do something
+		}
+	}
+
+	void Attack() {
+		timer = 0f;
+
+		if (catHealth.currentHealth > 0) {
+			catHealth.TakeDamage (attackDamage);
+		}
+	}
+}
