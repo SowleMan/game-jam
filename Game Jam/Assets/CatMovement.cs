e477b0fb545616bd999f9fa8332946d1ae7d5adc
SowleMan <tsowle1423@gmail.com>
2018-07-18 20:12:54 -0500

    Add: nave mesh and remove other project

---
+++ Game Jam/Assets/CatMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatMovement : MonoBehaviour {
+
+	public Transform building;
+	public UnityEngine.AI.NavMeshAgent agent;
+	public CatHealth catHealth;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (catHealth.currentHealth > 0) {
+			agent.SetDestination (building.position);
+		} else {
+			agent.enabled = false;
+		}
+	}
+}
